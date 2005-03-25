d95b6643b6d26b8ab0643a482daa8f887104c936
Paul Miller <jettero@voltar.org>
2005-03-25 19:41:33 +0000

    ok

--- MapGen.pm
+++ MapGen.pm
@@ -1,4 +1,4 @@
-# $Id: MapGen.pm,v 1.20 2005/03/25 19:41:33 jettero Exp $
+# $Id: MapGen.pm,v 1.24 2005/03/25 19:45:19 jettero Exp $
 # vi:tw=0 syntax=perl:
 
 package Games::RolePlay::MapGen::_group;
@@ -19,7 +19,7 @@ use strict;
 use AutoLoader;
 use Carp;
 
-our $VERSION = "0.07";
+our $VERSION = "0.11";
 our $AUTOLOAD;
 
 our %known_opts = (
