49d0157fc79b995ebb4a3c63fc7514baa7574efa
Paul Miller <jettero@voltar.org>
2005-03-30 20:53:21 +0000

    ok

--- go.sh
+++ go.sh
@@ -1,68 +1,44 @@
 #!/bin/bash
-# $Id: go.sh,v 1.21 2005/03/30 20:00:51 jettero Exp $
+# $Id: go.sh,v 1.22 2005/03/30 20:53:21 jettero Exp $
 # vi:tw=0:
 
 make || exit 1
 
 # perl -Iblib/lib -MExtUtils::Command::MM -e 'test_harness(0, "blib/lib", "blib/arch")' t/05_save*.t || exit 1
 
-rm -fv *.map
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
-perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
+# rm -fv *.map
+# perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" t/07*.t
 
-    #  # dprof= -d:DProf
-    #  perl $dprof -Iblib/lib -MGames::RolePlay::MapGen -e \
-    #  'my $map = new Games::RolePlay::MapGen({cell_size=>"20x20", num_rooms=>"3d8", bounding_box => 
-    #      # "3x3"
-    #      # "20x20"
-    #      "30x30"
-    #      # "63x22"
-    #      # "50x37"
-    #  }); 
-    #  
-    #  # set_generator $map("Perfect");
-    #  
-    #  generate $map; 
-    #  
-    #  set_visualization $map("Games::RolePlay::MapGen::Visualization::BasicImage");
-    #  visualize $map("map.png");
-    #  
-    #  set_visualization $map("Games::RolePlay::MapGen::Visualization::Text");
-    #  visualize $map("map.txt");
-    #  
-    #  ' || exit 1
-    #  
-    #  xv map.png &
+       # dprof= -d:DProf
+       perl $dprof -Iblib/lib -MGames::RolePlay::MapGen -e \
+       'my $map = new Games::RolePlay::MapGen({cell_size=>"20x20", 
+
+           num_rooms=>
+           "10d8", 
+           # "3d8", 
+
+           bounding_box => 
+           # "3x3"
+           # "20x20"
+           # "30x30"
+           # "63x22"
+           # "50x37"
+           "100x100"
+       }); 
+       
+       # set_generator $map("Perfect");
+       
+       generate $map; 
+       
+       set_visualization $map("Games::RolePlay::MapGen::Visualization::BasicImage");
+       visualize $map("map.png");
+       
+       set_visualization $map("Games::RolePlay::MapGen::Visualization::Text");
+       visualize $map("map.txt");
+       
+       ' || exit 1
+       
+       xv map.png &
 
 
 # perl -Iblib/lib -MExtUtils::Command::MM -e 'test_harness(0, "blib/lib", "blib/arch")' t/05*.t || exit 1
