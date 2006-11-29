e5a1d43f1445494aac41e963b4a6a64469a97ed2
Paul Miller <jettero@voltar.org>
2006-11-29 09:42:40 -0500

    ./ci

--- MapGen.pm
+++ MapGen.pm
@@ -7,7 +7,7 @@ use AutoLoader;
 use Carp;
 use Data::Dumper; $Data::Dumper::Indent = 1; $Data::Dumper::SortKeys = 1;
 
-our $VERSION = "0.29.1";
+our $VERSION = "0.29.2";
 our $AUTOLOAD;
 
 our %opp = (n=>"s", e=>"w", s=>"n", w=>"e");
