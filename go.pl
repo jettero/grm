8bbb695ce764e6a0deac7e6675a62d101a54c987
Paul Miller <jettero@voltar.org>
2007-05-12 07:18:25 -0400

    I moved all the object queue stuff to ../grd
    It's clearly part of a rules engine *NOT* a map generator.

--- go.pl
+++ go.pl
@@ -6,10 +6,8 @@ BEGIN { system("make || (perl Makefile.PL && make)") == 0 or die }
 use strict;
 use GD;
 use Games::RolePlay::MapGen;
-use Games::RolePlay::MapGen::Queue;
 
-&queue_play;
-# &std_generate;
+&std_generate;
 # &obr_generate;
 
 system("cp MapGen.dtd ~/www/MapGen.dtd") == 0 or die;
@@ -17,95 +15,6 @@ system("cp MapGen.xsl ~/www/MapGen.xsl") == 0 or die;
 system("cp map.xml    ~/www/MapGen.xml") == 0 or die;
 system("cp map.png    ~/www/MapGen.png") == 0 or die;
 system("chmod 644     ~/www/MapGen.*")   == 0 or die;
-
-sub queue_play {
-    my $map = new Games::RolePlay::MapGen({
-        tile_size    => 10,
-        cell_size    => "23x23", 
-        bounding_box => "15x15",
-        num_rooms    => "1d4",
-    });
-
-    warn "importing";
-    $map->set_generator( "XMLImport" );
-    $map->generate( xml_input_file => "vis1.xml" ); 
-
-    warn "drawing";
-    $map->set_exporter( "BasicImage" );
-    $map->export( "map.png" );
-
-    warn "marking";
-    my $queue = $map->queue;
-
-    warn "redir stderr>log";
-    open STDERR, ">log" or die $!;
-
-    # $queue->_lline_of_sight( [22,17]=>[21,16] );
-    # $queue->_lline_of_sight( [22,17]=>[24,21] );
-    # die;
-
-    my @things = map { my $b = $_; bless \$b, "Thing$b" } ( 1 .. 10 );
-
-    $queue->add( $_ => ($queue->random_open_location) ) for @things;
-
-    my @all = $queue->all_open_locations;
-     # @all = ([0,19], [1,19]);
-       @all = ([1,19], [3, 20]);
-
-    for my $dp (@all) {
-        my $image = GD::Image->new("map.png");
-        my $dude1 = $image->colorAllocate(100, 100, 255);
-        my $dude2 = $image->colorAllocate(0, 0, 0);
-        my $item1 = $image->colorAllocate(255, 255, 0);
-        my $item2 = $image->colorAllocate(0, 0, 0);
-
-        my $visible = $image->colorAllocate(220, 255, 220);
-        my $igcover = $image->colorAllocate(220, 220, 255);
-        my $cover   = $image->colorAllocate(255, 220, 220);
-        my $mcover  = $image->colorAllocate(220, 255, 220);
-
-        my @dude_position = @$dp;
-
-        # color visible tiles
-        for my $loc ($queue->locations_in_line_of_sight( @dude_position )) {
-            my $rc = $queue->ranged_cover( @dude_position => @$loc );
-            my $mc = $queue->melee_cover(  @dude_position => @$loc );
-
-            my $color = $visible;
-               $color = $cover   if $rc == LOS_COVER;
-               $color = $igcover if $rc == LOS_IGNORABLE_COVER;
-
-            $image->rectangle(23*$loc->[0]+1, 23*$loc->[1]+1, 23*($loc->[0]+1)-1, 23*($loc->[1]+1)-1, $color);
-            $image->fill(23*$loc->[0]+2, 23*$loc->[1]+2, $color); # the fill is separate because of doors
-
-            $image->rectangle(23*$loc->[0]+3, 23*$loc->[1]+3, 23*($loc->[0]+1)-3, 23*($loc->[1]+1)-3, $mcover) if $mc;
-        }
-
-        # draw things
-        for(@things) {
-            my $loc = $queue->location( $_ );
-            $image->filledEllipse(23*$loc->[0]+12, 23*$loc->[1]+12, 9, 9, $item1);
-            $image->ellipse(23*$loc->[0]+12, 23*$loc->[1]+12, 9, 9, $item2);
-        }
-
-        # draw the dude
-        $image->filledEllipse(23*$dude_position[0]+12, 23*$dude_position[1]+12, 9, 9, $dude1);
-        $image->ellipse(23*$dude_position[0]+12, 23*$dude_position[1]+12, 9, 9, $dude2);
-
-        open my $marked, ">marked.png";
-        print $marked $image->png;
-        close $marked;
-
-        EXEC: {
-            if(@all>5) {
-                system(qw(pngcrush marked.png), sprintf('marked_%02d-%02d.png', @$dp)) == 0 or exit 1;
-
-            } else {
-                system(qw(xv -geometry +0+0 marked.png)) == 0 or exit 1;
-            }
-        }
-    }
-}
 
 sub obr_generate {
     my $map = new Games::RolePlay::MapGen({
