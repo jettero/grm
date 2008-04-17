d97270c796f3622f5ada03146de19b2b3f2272e4
Paul Miller <jettero@voltar.org>
2008-04-17 09:11:52 -0400

    mmm that's maintainable

--- MapGen/Editor.pm
+++ MapGen/Editor.pm
@@ -4,6 +4,7 @@ package Games::RolePlay::MapGen::Editor;
 use strict;
 use Glib qw(TRUE FALSE);
 use Gtk2 -init; # -init tells import to ->init() your app
+use Gtk2::SimpleMenu;
 
 use constant {
     WINDOW => 0,
@@ -18,25 +19,54 @@ sub new {
     my $class = shift;
     my $this  = bless [], $class;
 
-    my $window    = $this->[WINDOW] = new Gtk2::Window ( "toplevel" );
+    my $window = $this->[WINDOW] = new Gtk2::Window("toplevel");
        $window->signal_connect( delete_event => sub { $this->quit } );
        $window->set_size_request(640,480); # TODO: the size and postion should have some kind of persistance
        $window->set_position('center');
 
-    my $main_vbox = Gtk2::VBox->new;         $window->add($main_vbox);
-    my $menu_bar  = Gtk2::MenuBar->new;      $main_vbox->pack_start($menu_bar, 0, 0, 0);
+    # NOTE: This awesome tree example comes from http://www.drdobbs.com/web-development/184416069
+    # (but it turns out that the example in the Gtk2::SimpleMenu pod is just as helpful)
 
-    my $File      = Gtk2::MenuItem->new('_File');
-    my $menu_file = Gtk2::Menu->new;
-       $menu_bar->append($File);
-       $File->set_submenu($menu_file);
+    my $menu_tree = [
+        _File => {
+            item_type => '<Branch>',
+            children => [
+                _Quit => {
+                    item_type   => '<StockItem>',
+                    callback    => sub { $this->quit },
+                    accelerator => '<ctrl>Q',
+                    extra_data  => 'gtk-quit',
+                },
+            ],
+        },
+        _Help => {
+            item_type => '<LastBranch>',
+            children => [
+                _Help => {
+                    item_type       => '<StockItem>',
+                    callback_action => 3,
+                    accelerator     => '<ctrl>H',
+                    extra_data      => 'gtk-help',
+                }
+            ],
+        },
+    ];
 
-    my $Exit = Gtk2::MenuItem->new('E_xit');
-       $Exit->signal_connect('activate' => sub { $this->quit });
-       $menu_file->append($Exit);
-    
+    my $menu = Gtk2::SimpleMenu->new (
+        menu_tree        => $menu_tree,
+        default_callback => sub { $this->unknown_menu_callback },
+    );
+
+    my $vbox = new Gtk2::VBox;
+       $vbox->pack_start($menu->{widget}, 0, 0, 0);
+
+    $window->add($vbox);
 
     return $this;
+}
+
+sub unknown_menu_callback {
+    my $this = shift;
 }
 
 sub quit {
