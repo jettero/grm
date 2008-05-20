64db37a6c8d2b2e2ef1a83bc8df29b0d1f00219d
Paul Miller <jettero@voltar.org>
2008-05-20 19:45:52 -0400

    forgot to reversion...

--- MapGen.pm
+++ MapGen.pm
@@ -7,7 +7,7 @@ use AutoLoader;
 use Carp;
 use Data::Dumper; $Data::Dumper::Indent = 1; $Data::Dumper::SortKeys = 1;
 
-our $VERSION = "1.2.15";
+our $VERSION = "1.2.16";
 our $AUTOLOAD;
 
 our %opp  = (n=>"s", e=>"w", s=>"n", w=>"e");
