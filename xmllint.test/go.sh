7058b50a32c8d5fcce06bab3cd64ced791e01212
Paul Miller <jettero@voltar.org>
2008-05-21 15:56:36 -0400

    woops

--- xmllint.test/go.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/usr/bin/xmllint --path t2 --postvalid --noout map.xml 2>/dev/null && echo test1: good
-/usr/bin/xmllint --path t2 --postvalid --noout bad.xml 2>/dev/null || echo test2: good
