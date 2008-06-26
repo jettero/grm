6ca7dedafea0854cf6b9cf3d658e6a4ec4fd5e5a
Paul Miller <paul@voltar.org>
2008-06-26 09:32:03 -0400

    this is what I'm submitting to perlbug

--- small.pl
+++ small.pl
@@ -3,24 +3,15 @@
 use strict;
 
 my $t = test->new("test");
-   $t->quantity(5);
-   $t->nonunique;
-   $t+=7;
-
-print "", $t+0, " =? 12\n";
+   $t->nonunique;  # comment out this line to avoid segmentation fault
+   $t+=7;       # or comment out this line to avoid segmentation fault
 
 package test;
 
 use strict;
-use overload fallback => 1, bool => sub{1}, '0+' => \&n, "-=" => \&me, "+=" => \&pe;
+use overload fallback => 1, bool => sub{1}, "+=" => \&pe;
 
-sub new { my $class = shift; my $val = shift; bless {q=>1, u=>1, v=>$val}, $class }
-
-sub n {
-    my $this = shift;
-
-    return $this->{q};
-}
+sub new { my $class = shift; my $val = shift; bless {q=>1, v=>$val}, $class }
 
 sub pe {
     my $this = shift;
@@ -29,21 +20,14 @@ sub pe {
     $this->{q} += $that;
 }
 
-sub me {
-    my $this = shift;
-    my $that = shift;
-    my $ordr = shift;
-
-    return ($this->{q} = ($that - $this->{q})) if $ordr;
-    $this->{q} -= $that;
-}
-
-sub quantity { my $this = shift; $this->{q}=0+shift; }
+sub quantity { my $this = shift; $this->{q} = shift }
 
 our %item_counts;
 sub nonunique { my $this = shift; $this->{c} = push @{$item_counts{$this->{v}}}, $this; }
 sub DESTROY {
     my $this = shift;
+
+    warn "we don't see this, unless you commented out one of the two lines";
 
     delete $item_counts{$this->{v}}[$this->{c}] if exists $this->{c};
 }
