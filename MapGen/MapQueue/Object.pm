68ef204fc2df3f134f7bc467684882dd8f588b29
Paul Miller <paul@voltar.org>
2008-06-25 21:52:50 -0400

    I think I completed the whole mqo... but I thought of another todo

--- MapGen/MapQueue/Object.pm
+++ MapGen/MapQueue/Object.pm
@@ -1,30 +1,51 @@
+# vi:filetype=perl:
 
 package Games::RolePlay::MapGen::MapQueue::Object;
 
 use strict;
-use overload fallback => 1, bool => \&b, '0+' => \&n, '""' => \&q;
+use overload fallback => 1, bool => sub{1}, '0+' => \&n, '""' => \&q;
 
-1;
+sub new { my $class = shift; my $val = shift; bless {q=>1, u=>1, v=>$val}, $class }
 
-sub new { my $class = shift; my $val = shift; bless {v=>$val}, $class }
+sub desc {
+    my $this = shift;
+
+    my $r = $this->{v};
+       $r .= " ($this->{q})" if $this->{q} != 1;
+       $r .= " #$this->{c}"  unless $this->{u};
+
+    return $r;
+}
 
 sub q {
     my $this = shift;
 
-    $this->{v};
+    return "$this->{v} #$this->{c}" unless $this->{u};
+    return $this->{v};
 }
 
 sub n {
     my $this = shift;
 
-    $this->{v};
+    return $this->{q};
 }
 
-sub b {
+our %item_counts;
+sub quantity  { my $this = shift; $this->{q}=0+shift; }
+sub    unique { my $this = shift; $this->{u}=1; }
+sub nonunique { my $this = shift; $this->{u}=0;
+    unless( exists $this->{c} ) {
+        $this->{c} = push @{$item_counts{$this->{v}}}, $this;
+    }
+}
+sub DESTROY {
     my $this = shift;
 
-    $this->{v};
+    delete $item_counts{$this->{v}}[$this->{c}] if exists $this->{c};
 }
+
+"I like GD::Graph.";
+
 
 __END__
 # Below is stub documentation for your module. You better edit it!
--- t/99_mqo.t
+++ t/99_mqo.t
@@ -6,7 +6,25 @@ use Games::RolePlay::MapGen::MapQueue::Object;
 my $s7 = new Games::RolePlay::MapGen::MapQueue::Object(7);
 my $st = new Games::RolePlay::MapGen::MapQueue::Object("test");
 
-plan tests => 2;
+plan tests => 11;
 
-ok($s7+0, 7);
+ok($s7+0, 1);
+ok($s7+2, 3);
+ok($s7/5, 1/5);
+
 ok($st, "test");
+
+$st->nonunique;
+ok($st, "test #1");
+
+$st->unique;
+ok($st, "test");
+
+$st->quantity(5);
+ok($st+3, 8);
+ok($st, "test");
+ok($st->desc, "test (5)");
+
+$st->nonunique;
+ok($st, "test #1");
+ok($st->desc, "test (5) #1");
