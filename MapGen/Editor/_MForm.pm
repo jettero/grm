5b43efc27f6fb0aad48c6ea3b6dd0c6e1ee31d7c
Paul Miller <paul@voltar.org>
2008-07-07 16:20:59 -0400

    started to modify _MF to take color input

--- MapGen/Editor/_MForm.pm
+++ MapGen/Editor/_MForm.pm
@@ -113,6 +113,49 @@ sub make_form {
 
               # $widget->signal_connect(changed => sub { warn "test!"; }); # [WORKS FINE]
 
+            } elsif( $IT eq "color" ) {
+                my $d = exists $i->{$item->{name}} ? $i->{$item->{name}} : $item->{default};
+                   $d = $item->{trevnoc}->($d) if ref $d and exists $item->{trevnoc};
+
+                $attach = $widget = new Gtk2::Button;
+                $widget->set_label($d);
+
+                $widget->set_tooltip_text( $item->{desc} ) if exists $item->{desc};
+                $
+
+                ### $widget->signal_connect(changed => sub {
+                ###     my $text = $widget->get_text;
+                ###     my $chg = 0;
+                ###     for my $fix (@{ $item->{fixes} }) {
+                ###         $chg ++ if $fix->($text);
+                ###     }
+                ###     $widget->set_text($text) if $chg;
+                ###     $req[$my_req] = 1;
+                ###     for my $match (@{ $item->{matches} }) {
+                ###         my $r = ref $match;
+                ###         if( $r eq "CODE" ) {
+                ###             $req[$my_req] = 0 unless $match->($text);
+
+                ###         } elsif( $r eq "Regexp" ) {
+                ###             $req[$my_req] = 0 unless $text =~ $match;
+
+                ###         } else {
+                ###             warn "bad match element for $item->{name}: $match";
+                ###         }
+                ###     }
+
+                ###     $dialog->set_response_sensitive( ok => (@req == grep {$_} @req) );
+                ### });
+
+                ### if( $item->{convert} ) {
+                ###     $item->{extract} = sub { $item->{convert}->( $widget->get_text ) };
+
+                ### } else {
+                ###     $item->{extract} = sub { $widget->get_text };
+                ### }
+
+              # $widget->signal_connect(changed => sub { warn "test!"; }); # [WORKS FINE]
+
             } elsif( $IT eq "choice" ) {
                 $attach = $widget = Gtk2::ComboBox->new_text;
                 my $d = $i->{$item->{name}} || $item->{default} || $item->{choices}[0];
