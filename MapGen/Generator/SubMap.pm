c5f037673815a4618bc71e444afa4a8ff51b423f
Paul Miller <paul@voltar.org>
2008-07-20 09:07:47 -0400

    this appears to function... we could probably use a test for it.

--- MapGen/Generator/SubMap.pm
+++ MapGen/Generator/SubMap.pm
@@ -19,15 +19,54 @@ sub genmap {
     my @X = ( sort {$a<=>$b} ($opts->{upper_left}[0], $opts->{lower_right}[0]) );
     my @Y = ( sort {$a<=>$b} ($opts->{upper_left}[1], $opts->{lower_right}[1]) );
 
-    for my $x ( 0 .. $X[1]+2 ) {
-        my $y = 0;       $map->[ $y ][ $x ] = &_tile( x=>$x, y=>$y, type=>'fog' );
-           $y = $Y[1]+2; $map->[ $y ][ $x ] = &_tile( x=>$x, y=>$y, type=>'fog' );
-    }
+    my $smap = $opts->{map_input}{_the_map};
 
-    for my $y ( 1 .. $Y[1]+1 ) {
-        my $x = 0;       $map->[ $y ][ $x ] = &_tile( x=>$x, y=>$y, type=>'fog' );
-           $x = $X[1]+2; $map->[ $y ][ $x ] = &_tile( x=>$x, y=>$y, type=>'fog' );
-    }
+    $opts->{$_} = $opts->{map_input}{$_} for qw(tile_size cell_size);
+    $opts->{x_size} = my $xs = 2+($X[1]-$X[0])+1;
+    $opts->{y_size} = my $ys = 2+($Y[1]-$Y[0])+1;
+    $opts->{bounding_box} = $xs . "x" . $ys;
+
+    for my $x ( 0 .. $xs-1 ) { my $y = 0;     $map->[ $y ][ $x ] = &_tile( x=>$x, y=>$y, type=>'fog' );
+                                  $y = $ys-1; $map->[ $y ][ $x ] = &_tile( x=>$x, y=>$y, type=>'fog' ); }
+
+    for my $y ( 1 .. $ys-2 ) { my $x = 0;     $map->[ $y ][ $x ] = &_tile( x=>$x, y=>$y, type=>'fog' );
+                                  $x = $xs-1; $map->[ $y ][ $x ] = &_tile( x=>$x, y=>$y, type=>'fog' ); }
+
+    ## DEBUG ## # disable mistakes:
+    ## DEBUG ## tie @$_, "Games::RolePlay::MapGen::_disallow_autoviv", $_ for $map, @$map;
+
+    for my $x1 ( $X[0] .. $X[1] ) { my $x2 = 1 + ($x1-$X[0]);
+    for my $y1 ( $Y[0] .. $Y[1] ) { my $y2 = 1 + ($y1-$Y[0]);
+        my $otile = $smap->[ $y1 ][ $x1 ];
+
+        my $ntile = $map->[ $y2 ][ $x2 ] = &_tile( x=>$x2, y=>$y2, (exists $otile->{type} ? (type=>$otile->{type}) :()) );
+        for my $d (qw(n e s w)) {
+            my $ood = $otile->{od}{$d};
+
+            my @ops = ($x2,$y2);
+               $ops[0] ++ if $d eq "e";
+               $ops[0] -- if $d eq "w";
+               $ops[1] ++ if $d eq "s";
+               $ops[1] -- if $d eq "n";
+
+            ## DEBUG ## do { local $" = ","; warn "($x2,$y2):$d -> (@ops)" };
+
+            my $opp = $Games::RolePlay::MapGen::opp{$d};
+            if( my $nt = $map->[ $ops[1] ][ $ops[0] ] ) {
+                if( ref $ood ) {
+                    $nt->{od}{$opp} = $ntile->{od}{$d} = &_door(
+                        map {($_ => $ood->{$_})} qw(locked stuck secret open),
+                        open_dir => { map {($ood->{open_dir}{$_})} qw(major minor) },
+                    );
+
+                } elsif($ood) {
+                    $nt->{od}{$opp} = $ntile->{od}{$d} = 1;
+                }
+            }
+        }
+    }}
+
+    $map = new Games::RolePlay::MapGen::_interconnected_map( $map );
 
     return ($map, $groups);
 }
